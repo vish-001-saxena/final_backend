afcbe263bc9d6c4d52f4492f1ec09774c42dfb76
vishi saxena <saxenavishi444@gmail.com>
2025-06-02 20:53:11 +0530

    models generate

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+#use to write structure 
+
+class Student(models.Model):
+    # id=models.AutoField()
+    name=models.CharField(max_length=100)
+    age=models.IntegerField()
+    email=models.EmailField()
+    address=models.TextField()
+    # image=models.ImageField()
+    # field=models.FieldFile()
+
+class product(models.Model):
+    pass
+
