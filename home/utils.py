78a83476df0bb45f33f0ee4ec2b4904ad478491b
vishi saxena <saxenavishi444@gmail.com>
2025-06-03 19:21:31 +0530

    shell file add

---
+++ home/utils.py
@@ -0,0 +1,9 @@
+from home.models import Student
+import time
+
+def run_this_function():
+    print("function Started")
+    print("function Started....")
+    print("function Started..............")
+    time.sleep(1)
+    print("function executed")
